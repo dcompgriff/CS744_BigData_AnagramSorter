344616a2241508e4f092e490b9732a19cd805197
vm@wisc.edu <vm@wisc.edu>
2017-09-16 01:42:51 +0000

    Updated reducer and sorter with new typedefs.

--- src/main/java/com/example/ac/AnagramReducer.java
+++ src/main/java/com/example/ac/AnagramReducer.java
@@ -9,16 +9,16 @@ import org.apache.hadoop.mapreduce.Reducer;
 /**
  * Reducer that concatenates all strings in a file to
  * */
-public class AnagramReducer extends Reducer<IntWritable,Text,NullWritable,Text> {
+public class AnagramReducer extends Reducer<Text,Text,Text,NullWritable> {
 	private Text result;
 	
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		// Combine the anagrams into a single group.
 		String groupedAnagrams = "";
 		for (Text val : values){
-			groupedAnagrams += val.toString() + " ";
+			groupedAnagrams += val.toString() + ", ";
 		}
 		result = new Text(groupedAnagrams);
-		context.write(NullWritable.get(), result);
+		context.write(result, NullWritable.get());
 	}
 }
